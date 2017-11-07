88ab25982b9546bc3fce3bd7d184ea0ef33217ac
nazmul0637 <bsse0637@iit.du.ac.bd>
2017-11-07 18:55:21 +0600

    Update reducer.py

---
+++ assignment3/reducer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import sys
+dict = {}
+for line in sys.stdin:
+	file_path = line.strip()
+	dict[file_path] = dict.get(file_path, 0) + 1
+file_path_that_max_hitted = max(dict, key=dict.get)
+print str(file_path_that_max_hitted)
